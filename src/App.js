bf253078e2e4c50e64f5f7ebb86bb7b63911abf7
shwetaps605 <shwetaroy305@gmail.com>
2022-03-21 22:21:43 +0530

    added NavBar component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import {Layout, Typography, Space} from 'antd'
 const App = () => {
 
     return(
-        <div>
-            <h1>CryptoVerse</h1>
+        <div className='app'>
+            <div className='navbar'>
+
+            </div>
+            <div className='main'>
+                
+            </div>
+            <div className='footer'>
+                
+            </div>
         </div>
     )
     
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+
+
+const NavBar = () => {
+    return(
+        <div>
+
+        </div>
+    )
+
+
+}
+
+export default NavBar;
