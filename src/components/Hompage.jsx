456d9de02b8cbab9afc4cae42928d2a9bfe5b407
shwetaps605 <shwetaroy305@gmail.com>
2022-03-23 22:25:24 +0530

    added code for Homepage component

--- src/components/Hompage.jsx
+++ src/components/Hompage.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import {Link} from 'react-router-dom'
+
+const { Title } = Typography
 
 const Hompage = () => {
   return (
-    <div>Hompage</div>
+    <>
+        <Title level={2} className='heading'>
+            Global Crypto Stats
+        </Title>
+        {/* in antd there are a total of 24 spaces */}
+        <Row>
+            <Col span={12}>
+                <Statistic title="Total Cryptocurrencies" value="5"/>
+            </Col>
+            <Col span={12}>
+                <Statistic title="Total Exchanges" value="5"/>
+            </Col>
+            <Col span={12}>
+                <Statistic title="Total Market Cap" value="5"/>
+            </Col>
+            <Col span={12}>
+                <Statistic title="Total 24h volume" value="5"/>
+            </Col>
+            <Col span={12}>
+                <Statistic title="Total Markets" value="5"/>
+            </Col>
+        </Row>
+    </>
   )
 }
 
