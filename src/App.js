e287fbeed298d2aaa15fc4ed25c25633390b5759
shwetaps605 <shwetaroy305@gmail.com>
2022-03-23 22:04:31 +0530

    added the components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route, Link} from 'react-router-dom'
 import {Layout, Typography, Space} from 'antd'
 
-import { NavBar } from './components'
+import { NavBar, Homepage, Exchanges, News, Cryptocurrencies, CryptoDetails} from './components'
 import './App.css'
 
 const App = () => {
@@ -13,6 +13,27 @@ const App = () => {
                 <NavBar/>
             </div>
             <div className='main'>
+                <Layout>
+                    <div className='router'>
+                        <Switch>
+                            <Route exactly path='/'>
+                                <Homepage/>
+                            </Route>
+                            <Route exactly path='/exchanges'>
+                                <Exchanges/>
+                            </Route>
+                            <Route exactly path='/cryptocurrencies'>
+                                <Cryptocurrencies/>
+                            </Route>
+                            <Route exactly path='/crypto/:coinId'>
+                                <CryptoDetails/>
+                            </Route>
+                            <Route exactly path='/news'>
+                                <News/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
                 
             </div>
             <div className='footer'>
---
+++ src/components/Hompage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hompage = () => {
+  return (
+    <div>Hompage</div>
+  )
+}
+
+export default Hompage
