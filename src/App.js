f86251a4dc499328e7452247890d969744a62a01
shwetaps605 <shwetaroy305@gmail.com>
2022-03-23 21:33:37 +0530

    added the menu items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Link} from 'react-router-dom'
 import {Layout, Typography, Space} from 'antd'
 
 import { NavBar } from './components'
+import './App.css'
 
 const App = () => {
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,13 +8,31 @@ const NavBar = () => {
     return(
         <div className='navbar-container'>
             <div className="log-container">
-                <Avatar src = {icon} size='large'/>
+                <Avatar src = {icon} size='small'/>
                 <Typography.Title level={2} className='logo'>
-                    {/* <Link to='/'>CryptoVerse</Link> */}
+                    <Link to='/'>CryptoVerse</Link>
                 </Typography.Title>
                 {/* <Button className='menu-control-container'>
 
                 </Button> */}
+                <Menu theme='dark'>
+
+                    <Menu.Item icon={<HomeOutlined/>}>
+                        <Link to='/'>Home</Link>
+                    </Menu.Item>
+
+                    <Menu.Item icon={<FundOutlined/>}>
+                        <Link to='/cryptocurrencies'>Cryptocurrency</Link>
+                    </Menu.Item>
+
+                    <Menu.Item icon={<MoneyCollectOutlined/>}>
+                        <Link to='/exchanges'>Exchanges</Link>
+                    </Menu.Item>
+
+                    <Menu.Item icon={<BulbOutlined/>}>
+                        <Link to='/news'>News</Link>
+                    </Menu.Item>
+                </Menu>
             </div>
 
         </div>
