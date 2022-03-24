a1d3888c6c622eb2ee2d6232fd01587a36e6c489
shwetaps605 <shwetaroy305@gmail.com>
2022-03-25 00:13:08 +0530

    got the first api working

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery} from '@redux/toolkit/query'
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
     'X-RapidAPI-Key': '43751a6cedmshaede457539899bfp16f453jsn7f540c04e2e0'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 //a custom fucntion which takes a url and adds the headers to it
 const createRequest = (url) => ({ url, headers:cryptoApiHeaders})
@@ -15,7 +15,9 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints: (builder) =>  ({
         getCryptos: builder.query({
-            query:() => createRequest('/exchanges')
+            query:() => createRequest('/coins')
         })
     })
 })
+
+export const {useGetCryptosQuery} = cryptoApi
