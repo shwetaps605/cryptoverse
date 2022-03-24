fde588e73a22f0bf805101696d6ee6fe12883ffd
shwetaps605 <shwetaroy305@gmail.com>
2022-03-25 00:20:49 +0530

    added the dynamic values in the stats in HomePage component

--- src/components/Hompage.jsx
+++ src/components/Hompage.jsx
@@ -10,8 +10,14 @@ const { Title } = Typography
 const Hompage = () => {
 
     const { data, isFetching } = useGetCryptosQuery()
+    const globalStats = data?.data?.stats;
+
     console.log(data);
-    
+
+    if(isFetching) return 'Loading...'
+
+
+
   return (
     <>
         <Title level={2} className='heading'>
@@ -20,19 +26,19 @@ const Hompage = () => {
         {/* in antd there are a total of 24 spaces */}
         <Row>
             <Col span={12}>
-                <Statistic title="Total Cryptocurrencies" value="5"/>
+                <Statistic title="Total Cryptocurrencies" value={globalStats.total}/>
             </Col>
             <Col span={12}>
-                <Statistic title="Total Exchanges" value="5"/>
+                <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/>
             </Col>
             <Col span={12}>
-                <Statistic title="Total Market Cap" value="5"/>
+                <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/>
             </Col>
             <Col span={12}>
-                <Statistic title="Total 24h volume" value="5"/>
+                <Statistic title="Total 24h volume" value={millify(globalStats.total24hVolume)}/>
             </Col>
             <Col span={12}>
-                <Statistic title="Total Markets" value="5"/>
+                <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/>
             </Col>
         </Row>
     </>
