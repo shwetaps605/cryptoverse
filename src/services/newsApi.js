fa91a6aa59fdaf5a2c3ef7b33c2afab4479ff202
shwetaps605 <shwetaroy305@gmail.com>
2022-03-28 22:27:36 +0530

    started working on newsApi

---
+++ src/services/newsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const newsApiHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': '43751a6cedmshaede457539899bfp16f453jsn7f540c04e2e0'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) = ({ url, headers: newsApiHeaders})
+
+export const newsApi = createApi({
+    reducerPath:'cryptoNewsApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: () => createRequest(``)
+        })
+    })
+
+})
